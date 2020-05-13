c4d4ca8a8097586489ee0e2359e35a0f0543db34
Cosingf <1109400576@qq.com>
2020-05-13 20:32:14 +0800

    delete DosSystem

--- DocSystem/legaldocument/src/main/java/cn/xmu/edu/legaldocument/entity/EntityType.java
+++
@@ -1,7 +0,0 @@
-package cn.xmu.edu.legaldocument.entity;
-
-public class EntityType {
-    public static int ENTITY_DISCUSS= 1;
-    public static int ENTITY_COMMENT = 2;
-    public static int ENTITY_USER = 3;
-}
